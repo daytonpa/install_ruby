24dd94b5c9ce474a20abdc2533ad31065442988a
Patrick Dayton <pdayton@fastrobot.com>
2017-02-15 10:50:04 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# install_ruby
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'install_ruby'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures install_ruby'
+long_description 'Installs/Configures install_ruby'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/install_ruby/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/install_ruby' if respond_to?(:source_url)
